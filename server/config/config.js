fb20e76c85336ed84c2004d7ff0edd822d5dd829
Yoana Zelova <y_zelova@outlook.com>
2019-04-11 20:14:32 +0300

    modifications for hosting

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ const session = require('express-session');
 const csrf = require('csurf');
 const cors = require('cors')
 
-module.exports = (app) => {
+module.exports = async (app) => {
     app.use(cors());
     app.use(cookieParser());
     app.use(session({
@@ -20,6 +20,29 @@ module.exports = (app) => {
     app.use(passport.session());
 
     const csrfProtection = csrf({cookie: true});
+    
+    await sequelize.query(`CREATE TABLE IF NOT EXISTS users
+    (
+        id serial NOT NULL,
+        first_name text COLLATE pg_catalog."default",
+        last_name text COLLATE pg_catalog."default",
+        email text COLLATE pg_catalog."default" NOT NULL,
+        password text COLLATE pg_catalog."default" NOT NULL,
+        CONSTRAINT users_pkey PRIMARY KEY (id)
+    )`);
+
+    await sequelize.query(`CREATE TABLE IF NOT EXISTS user_image
+    (
+        id serial NOT NULL,
+        user_id bigint NOT NULL,
+        image_name text COLLATE pg_catalog."default" NOT NULL,
+        created_at timestamp with time zone,
+        CONSTRAINT user_image_pkey PRIMARY KEY (id),
+        CONSTRAINT user_image_user_id_fkey FOREIGN KEY (user_id)
+            REFERENCES users (id) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
+    )`);
 
     const ormModels = require('../orm_models/index')(sequelize);
     const models = require('../models/index')(ormModels);
