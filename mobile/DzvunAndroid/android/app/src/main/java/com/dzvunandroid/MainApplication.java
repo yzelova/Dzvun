556f3432d280286ad85e519dfdd43fe80c2defe2
bokoto000 <bobi_velk@abv.bg>
2019-04-26 17:23:43 +0300

    Android Update

--- mobile/DzvunAndroid/android/app/src/main/java/com/dzvunandroid/MainApplication.java
+++ mobile/DzvunAndroid/android/app/src/main/java/com/dzvunandroid/MainApplication.java
@@ -3,6 +3,7 @@ package com.dzvunandroid;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.polidea.reactnativeble.BlePackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BlePackage(),
             new RNFirebasePackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
