4f3b734658c9cc1f443a24bd823750673d9ad33b
Yoana Zelova <y_zelova@outlook.com>
2019-05-08 23:01:28 +0300

    changes

--- server/orm_models/index.js
+++ server/orm_models/index.js
@@ -2,11 +2,18 @@ module.exports =  (sequelize) => {
     const User = require('./user')(sequelize);
     const UserImage = require('./user_image')(sequelize);
     const Device = require('./device')(sequelize);
+    const UserDevice = require('./user_device')(sequelize);
+    User.hasMany(UserImage);
     UserImage.belongsTo(User);
+
+    User.hasMany(UserDevice);
+    UserDevice.belongsTo(User);
+    UserDevice.belongsTo(Device);
     
     return {
         User,
         UserImage,
-        Device
+        Device, 
+        UserDevice
     }
 }
